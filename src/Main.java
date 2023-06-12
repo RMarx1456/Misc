56e2a959a7d171fd5fb7e2fe109e9e30ebd90a3e
hello <hellohi12465@gmail.com>
2023-06-11 22:13:16 -0700

    Committing driver file.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+/*
+ * Entry point
+ * @author RMarx1456
+ * @version 1.0
+ *
+ */
 public class Main {
+    /**
+     * Starts the application
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+
         System.out.println("Hello world...");
     }
 }
